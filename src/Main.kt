eb5bc4658322b5963a2d7364af3adfb31dea276a
SiJibrilll <jibrilianwa@gmail.com>
2025-07-05 14:34:01 +0700

    added a main system class an d main menu

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,7 @@ import kotlin.text.toInt
 
 class Sales(var value:Long = 0, var date:Int = 1, var month:Int = 1,  var year: Int = 1 )
 
-class System {
+class SalesManager {
     val sales = mutableListOf<Sales>()
 
     fun addSales( value:Long = 0,  date:Int = 1, month:Int = 1,  year: Int = 1) {
@@ -16,7 +16,7 @@ class System {
 
         for ((i, column) in collumns.withIndex()) {
             when (column) {
-                "value" -> sales[index].value = if (inputs[i] == -1) sales[index].value else inputs[i].toLong()
+                "value" -> sales[index].value = if (inputs[i] == -1L) sales[index].value else inputs[i].toLong()
                 "date" -> sales[index].date = if (inputs[i] == -1) sales[index].date else inputs[i].toInt()
                 "month" -> sales[index].month = if (inputs[i] == -1) sales[index].month else inputs[i].toInt()
                 "year" -> sales[index].year = if (inputs[i] == -1) sales[index].year else inputs[i].toInt()
@@ -49,6 +49,160 @@ class System {
     }
 }
 
+class System {
+    val sales = SalesManager()
+    
+
+    fun salesManager() {
+        val salesMenus = arrayOf("index", "add", "update", "delete", "back")
+
+        while (true) {
+            clearConsole()
+            
+            println("= = = = INTRANATIONAL BUSSINESS MACHINE = = = =")
+            println("- - - Sales Menu")
+        
+            for ((i, menu) in salesMenus.withIndex()) {
+                println("[$i] $menu")
+            }
+            line()
+            println("Please select a menu: ")
+            var select = readLine()
+
+            clearConsole()
+        
+            if (select == null || select.equals("")) {
+                println("Invalid menu selected")
+                confirm()
+                continue
+            }
+        
+            
+        
+            when (salesMenus[select.toInt()]) {
+                "index" -> {
+                    sales.indexSales()
+                    line()
+                    confirm()
+                }
+
+                "add" -> {
+                    var input:String
+                    var error = false
+
+                    var value:Long
+                    var date:Int
+                    var month:Int
+                    var year:Int
+
+                    println("Input sale value: ")
+                    value = readLine()?.toLongOrNull() ?: -1L
+
+                    println("Input date value: ")
+                    date = readLine()?.toIntOrNull() ?: -1
+                
+                    println("Input month value: ")
+                    month = readLine()?.toIntOrNull() ?: -1
+                
+                    println("Input year value: ")
+                    year = readLine()?.toIntOrNull() ?: -1
+
+                    if (value < 0L || date < 0 || month < 0 || year < 0) {
+                        println("Invalid inputs")
+                        confirm()
+                        continue
+                    }
+
+                    sales.addSales(value, date, month, year)
+
+                    clearConsole()
+                    println("Sale added: ")
+                    println("Sale value of: $value in $date/$month/$year")
+                    line()
+                    confirm()
+                
+                }
+
+                "update" -> {
+                    sales.indexSales()
+                    line()
+                    println("Select index of sale to edit: ")
+                    var input = readLine()?.toIntOrNull() ?: -1
+
+                    clearConsole()
+
+                    if (input == -1) {
+                        println("Invalid index!")
+                        line()
+                        confirm()
+                        continue
+                    }
+
+                    var value:Long
+                    var date:Int
+                    var month:Int
+                    var year:Int
+                    
+                    var sale = sales.getSales(input)
+                    println("Input new sale value: (Leave empty to use old data: ${sale.value})")
+                    value = readLine()?.toLongOrNull() ?: -1L
+
+                    println("Input new date value: (Leave empty to use old data ${sale.date})")
+                    date = readLine()?.toIntOrNull() ?: -1
+                
+                    println("Input new month value: (Leave empty to use old data: ${sale.month})")
+                    month = readLine()?.toIntOrNull() ?: -1
+                
+                    println("Input new year value: (Leave empty to use old data: ${sale.year})")
+                    year = readLine()?.toIntOrNull() ?: -1
+
+                    sales.updateSales(input, value, date, month, year)
+
+                    clearConsole()
+                    println("Sale updated!")
+
+                    sale = sales.getSales(input)
+                    println("[$input] Sale of: ${sale.value} in ${sale.date}/${sale.month}/${sale.year}")
+                    line()
+                    confirm()
+                    continue
+                }
+
+                "delete" -> {
+                    println("Select a sale index to delete")
+                    sales.indexSales()
+                    line()
+                    var input = readLine()?.toIntOrNull() ?: -1
+
+                    clearConsole()
+
+                    
+                    if (input == -1 || input >= sales.sales.size) {
+                        println("Invalid index")
+                        line()
+                        confirm()
+                        continue
+                    }
+
+                    var sale = sales.getSales(input)
+                    sales.deleteSales(input)
+
+                    println("Delete completed!")
+                    println("Sale [$input] of: ${sale.value} in ${sale.date}/${sale.month}/${sale.year} has been deleted!")
+                    line()
+                    confirm()
+                }
+
+                "back" -> {
+                    break
+                }
+        
+            }
+        }
+    }
+    
+}
+
 fun clearConsole() {
     val os = System.getProperty("os.name") // Make sure 'System.' is here
     if (os.contains("Windows", ignoreCase = true)) {
@@ -68,149 +222,36 @@ fun confirm() {
 }
 fun main() {
     val system = System()
-    val menus = arrayOf("index", "add", "update", "delete", "exit")
+    val mainMenus = arrayOf("Manage Sales", "exit")
 
     while (true) {
         clearConsole()
-        
         println("= = = = INTRANATIONAL BUSSINESS MACHINE = = = =")
-    
-        for ((i, menu) in menus.withIndex()) {
+            
+        for ((i, menu) in mainMenus.withIndex()) {
             println("[$i] $menu")
         }
         line()
         println("Please select a menu: ")
-        var select = readLine()
+        var select = readLine()?.toIntOrNull() ?: -1
 
-        clearConsole()
-    
-        if (select == null || select.equals("")) {
-            println("Invalid menu selected")
+        if (select == -1) {
+            println("Invalid selection!")
+            line()
             confirm()
             continue
         }
-    
-        
-    
-        when (menus[select.toInt()]) {
-            "index" -> {
-                system.indexSales()
-                line()
-                confirm()
-            }
 
-            "add" -> {
-                var input:String
-                var error = false
 
-                var value:Long
-                var date:Int
-                var month:Int
-                var year:Int
-
-                println("Input sale value: ")
-                value = readLine()?.toLongOrNull() ?: -1L
-
-                println("Input date value: ")
-                date = readLine()?.toIntOrNull() ?: -1
-              
-                println("Input month value: ")
-                month = readLine()?.toIntOrNull() ?: -1
-              
-                println("Input year value: ")
-                year = readLine()?.toIntOrNull() ?: -1
-
-                if (value < 0L || date < 0 || month < 0 || year < 0) {
-                    println("Invalid inputs")
-                    confirm()
-                    continue
-                }
-
-                system.addSales(value, date, month, year)
-
-                clearConsole()
-                println("Sale added: ")
-                println("Sale value of: $value in $date/$month/$year")
-                line()
-                confirm()
-              
-            }
-
-            "update" -> {
-                system.indexSales()
-                line()
-                println("Select index of sale to edit: ")
-                var input = readLine()?.toIntOrNull() ?: -1
-
-                clearConsole()
-
-                if (input == -1) {
-                    println("Invalid index!")
-                    line()
-                    confirm()
-                    continue
-                }
-
-                var value:Long
-                var date:Int
-                var month:Int
-                var year:Int
-                
-                var sale = system.getSales(input)
-                println("Input new sale value: (Leave empty to use old data: ${sale.value})")
-                value = readLine()?.toLongOrNull() ?: -1L
-
-                println("Input new date value: (Leave empty to use old data ${sale.date})")
-                date = readLine()?.toIntOrNull() ?: -1
-              
-                println("Input new month value: (Leave empty to use old data: ${sale.month})")
-                month = readLine()?.toIntOrNull() ?: -1
-              
-                println("Input new year value: (Leave empty to use old data: ${sale.year})")
-                year = readLine()?.toIntOrNull() ?: -1
-
-                system.updateSales(input, value, date, month, year)
-
-                clearConsole()
-                println("Sale updated!")
-
-                sale = system.getSales(input)
-                println("[$input] Sale of: ${sale.value} in ${sale.date}/${sale.month}/${sale.year}")
-                line()
-                confirm()
-                continue
-            }
-
-            "delete" -> {
-                println("Select a sale index to delete")
-                system.indexSales()
-                line()
-                var input = readLine()?.toIntOrNull() ?: -1
-
-                clearConsole()
-
-                
-                if (input == -1 || input >= system.sales.size) {
-                    println("Invalid index")
-                    line()
-                    confirm()
-                    continue
-                }
-
-                var sale = system.getSales(input)
-                system.deleteSales(input)
-
-                println("Delete completed!")
-                println("Sale [$input] of: ${sale.value} in ${sale.date}/${sale.month}/${sale.year} has been deleted!")
-                line()
-                confirm()
-            }
-
+        when (mainMenus[select]) {
+            "Manage Sales" -> system.salesManager()
             "exit" -> {
                 println("Good Bye!")
                 break
             }
-    
         }
+
     }
+
+
 }
